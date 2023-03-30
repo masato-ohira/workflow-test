f7af9251342bafc83a746c88b905e1862132c7ee
Masato Ohira <ohira.ekzm@gmail.com>
2023-03-30 15:14:11 +0900

    fix

--- npm-scripts/slack.js
+++ npm-scripts/slack.js
@@ -3,7 +3,6 @@ const { IncomingWebhook } = require('@slack/webhook')
 require('dotenv').config()
 const webhook = new IncomingWebhook(SLACK_HOOK)
 const main = async (text) => {
-  console.log({ SLACK_HOOK })
   await webhook.send({ text })
   return true
 }
